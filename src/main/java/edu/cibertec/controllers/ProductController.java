43cca8446c7d1a4891da9b2237fb25cef81d5bc5
Alex Marin Mendez <alexmarinmendez@gmail.com>
2025-02-16 08:25:04 -0500

    feat: add POST /products

--- src/main/java/edu/cibertec/controllers/ProductController.java
+++ src/main/java/edu/cibertec/controllers/ProductController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.client.RestTemplate;
 
 import edu.cibertec.dtos.ProductDTO;
@@ -34,6 +35,20 @@ public class ProductController {
 		ResponseEntity<ProductDTO> product = this.restTemplate.getForEntity(this.apiUrl + '/' + id, ProductDTO.class);
 		model.addAttribute("producto", product.getBody());
 		return "detalle-producto";
+	}
+	
+	@GetMapping("/nuevo")
+	public String showFormToRegister(Model model) {
+		model.addAttribute("producto", new ProductDTO());
+		return "formulario-nuevo";
+	}
+	
+	@PostMapping("/grabar")
+	public String save(ProductDTO productDTO, Model model) {
+		this.restTemplate.postForEntity(this.apiUrl, productDTO, ProductDTO.class);
+		ResponseEntity<ProductDTO[]> products = this.restTemplate.getForEntity(this.apiUrl, ProductDTO[].class);
+		model.addAttribute("productos", products.getBody());
+		return "productos";
 	}
 	
 }
